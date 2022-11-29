c88cf97b23bc24a94b7d004c68f62804fbcb8619
cedricimt <cedric.diollot@imt-atlantique.net>
2022-11-29 14:43:22 +0100

    test pub et sub

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	// "token"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
 func main() {
-	fmt.Println("hello world")
+	//1 connection
+	client := connect("tcp://localhost:1883", "toto")
+	//2 subscription
+	client.Subscribe("/topic1", 0, nil)
+	//3 publication
+	token := client.Publish("topic1", 0, false, "ceci est mon ")
+
+	
+	token.Wait()
+}
+
+func test() string {
+	return "1"
+}
+
+func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(brokerURI)
+
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func connect(brokerURI string, clientId string) mqtt.Client {
+	fmt.Println("Tentative de connection ( " + brokerURI + ", " + clientId + ") ...")
+	opts := createClientOptions(brokerURI, clientId)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
 }
