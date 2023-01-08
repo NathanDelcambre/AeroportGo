682a36cb21661872c379c1a1525175d145fc6914
cedricmobidys <cedric.diollot@mobidys.fr>
2023-01-08 16:19:45 +0100

    bug de boucle sur fichier de conf

--- cmd/publisher/publisher.go
+++ cmd/publisher/publisher.go
@@ -1,31 +1,89 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"os"
+	"reflect"
 	"strconv"
 	"time"
-	"archi.org/aeroportGo/internal/sensors"
-	"archi.org/aeroportGo/internal/connection/mosquitto"
 
+	"archi.org/aeroportGo/internal/connection/mosquitto"
+	"archi.org/aeroportGo/internal/sensors"
 )
 
-// TO DO rajouter QoS dans args
+type Configuration struct {
+	// Words []string `json:"words"`
+	IATA       []string `json:"iata"`
+	SensorID   []int    `json:"sensorId"`
+	SensorType []string `json:"sensorType"`
+}
+
+func contains(val interface{}, arr interface{}) bool {
+	// Vérifier si la variable est présente dans le tableau
+	arrValue := reflect.ValueOf(arr)
+	for i := 0; i < arrValue.Len(); i++ {
+		if arrValue.Index(i).Interface() == val {
+			return true
+		}
+	}
+	return false
+}
 
 /**
-	.\publisher.exe [ipAndPort] [sensorId] [iata] [sensorType]
+	.\publisher.exe [ipAndPort] [sensorId] [iata] [sensorType] [QoS]
 
 	@ipAndPort: localhost:1883
 	@sensorId: 1, 2 or 3
 	@iata: cf wikipedia
 	@sensorType: Wind, Heat, Pressure
+	@QoS: 2
 
 	topic => airports/iata/sensorType/
 	données => sensorId:iata:sensorType:valeur:YYYY-MM-DD-mm-ss
 **/
 
 func main() {
+	// test read config files
+	// data, err := ioutil.ReadFile("../../internal/conf/conf.json")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// var config Configuration
+	// err = json.Unmarshal(data, &config)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// for _, word := range config.IATA {
+	// 	fmt.Println(word)
+	// }
+
+	file, err := os.Open("../../internal/conf/conf.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	data := Configuration{}
+
+	err = json.NewDecoder(file).Decode(&data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(contains("CDG", data.IATA))
+	// for i := 0; i < len(data.IATA); i++ {
+		
+	// 	fmt.Println(i, data.IATA[i])
+	// }
+
 	var start time.Time
 	var duration float64
 	var timeToWait time.Duration
@@ -35,16 +93,29 @@ func main() {
 
 	args := os.Args
 	ipAndPort := args[1]
-	sensorId, err := strconv.Atoi(args[2])
+	sensorId, errSensor := strconv.Atoi(args[2])
 	iata := args[3]
 	sensorType := args[4]
+	qualityOfService, errQoS := strconv.Atoi(args[5])
+
+	fmt.Println(qualityOfService)
 
 	client := mosquitto.Connect("tcp://" + ipAndPort, "123")
 
-	if err != nil {
-		fmt.Println(err)
+	if errSensor != nil {
+		fmt.Println(errSensor)
 		fmt.Println(" => le type de l'ID n'est pas un int")
 		os.Exit((1))
+	}
+
+	if errQoS != nil {
+		fmt.Println(errQoS)
+		fmt.Println("le type du QoS n'est pas bon, utilisez un entier")
+		os.Exit(1)
+	}
+
+	if qualityOfService != 2 {
+		fmt.Println("Le QoS n'est pas à 2, nous suggérons de relancer l'application en utilisant 2 pour un meilleur QoS")
 	}
 
 	switch sensorType {
@@ -66,7 +137,7 @@ func main() {
 
 		token := client.Publish(
 			"airports/" + iata + "/" + sensorType,
-			2,
+			byte(qualityOfService),
 			false,
 			strconv.Itoa(sensorId) + ":" + iata + ":"  + sensorType + ":"  + strconv.FormatFloat(value, 'f', -1, 64) + ":"  + date,
 		)
