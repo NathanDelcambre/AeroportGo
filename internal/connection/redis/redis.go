645abf1455bbc1e08fb6772dca5e0d8632798c21
technalp <pro.planchet@gmail.com>
2023-01-10 15:50:39 +0100

    Gros merge

--- internal/connection/redis/redis.go
+++ internal/connection/redis/redis.go
@@ -7,15 +7,21 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
+type Conn redis.Conn
+
 func ConnectRedis(host string) redis.Conn {
-	fmt.Println("Tentative de connexion à REDIS: " + host)
+	fmt.Print("Tentative de connexion à REDIS: " + host + "...")
 	conn, err := redis.Dial("tcp", host)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("OK")
 	return conn
 }
 
 func DisconnetRedis(conn redis.Conn) {
+	if conn == nil {
+		log.Fatal("Impossible de fermer la connexion à REDIS")
+	}
 	conn.Close()
 }
